03b8c10d330fd0ec44ef98159ea0c2e849d4e47f
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-08-28 20:41:06 -0400

    fix idioms

--- public_opinion/gather_tweet_data.py
+++ public_opinion/gather_tweet_data.py
@@ -1,43 +1,47 @@
-import snscrape.modules.twitter as sntwitter
-import pandas as pd
+def main():
+    import snscrape.modules.twitter as sntwitter
+    import pandas as pd
+    
+    #transpose a matrix
+    def transpose(matrix):
+        num_rows = len(matrix[0])
+        num_columns = len(matrix)
+        Final = [[0]*num_columns for x in range(num_rows)]
+        for x in range(num_rows):
+            for i in range(num_columns):
+                Final[x][i] = matrix[i][x]
+        return Final
+    
+    #change an int to a string. if it's only made up of one character,
+    #add a zero to the beginning. (this is because of the way twitter 
+    #handles dates)
+    def add_zero(date):
+        date = str(date)
+        if len(date) == 1:
+            date = '0' + date
+        return date
+    
+    companies = ["Tesla", "Microsoft", "Google"]
+    
+    #get a range of numbers from 1 to 11 for the months we're gathering data on
+    start_dates = [x+1 for x in range(11)]
+    
+    limit = 1000 #the number of tweets collected per company per month
+    for company in companies:
+        tweets = []
+        for date_index, start_date in enumerate(start_dates):
+            end_date = start_date + 1 #the end data is one month after the start date
+            start_date = add_zero(start_date) #convert to str and add 0 to start if one digit
+            end_date = add_zero(end_date) #same as above
+            query = f"{company} lang:en until:2021-{end_date}-01 since:2021-{start_date}-01" #the query submitted to twitter
+            for index, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+                tweets.append([date_index+1, tweet.content]) #appends each tweet we get to the list with the month it's from
+                if index == limit-1: #stop once the limit has been reached
+                    break
+        tweets = transpose(tweets) #pandas wants them transposed
+        tweet_df = pd.DataFrame(tweets, ['Month', 'Content'])
+        tweet_df = tweet_df.transpose() #transpose the df to get them back to how they were
+        tweet_df.to_csv(f'{company}.csv') #write to output file
 
-#transpose a matrix
-def transpose(matrix):
-    num_rows = len(matrix[0])
-    num_columns = len(matrix)
-    Final = [[0]*num_columns for x in range(num_rows)]
-    for x in range(num_rows):
-        for i in range(num_columns):
-            Final[x][i] = matrix[i][x]
-    return Final
-
-#change an int to a string. if it's only made up of one character,
-#add a zero to the beginning. (this is because of the way twitter 
-#handles dates)
-def add_zero(date):
-    date = str(date)
-    if len(date) == 1:
-        date = '0' + date
-    return date
-
-companies = ["Tesla", "Microsoft", "Google"]
-
-#get a range of numbers from 1 to 11 for the months we're gathering data on
-start_dates = [x+1 for x in range(11)]
-
-limit = 1000 #the number of tweets collected per company per month
-for company in companies:
-    tweets = []
-    for date_index, start_date in enumerate(start_dates):
-        end_date = start_date + 1 #the end data is one month after the start date
-        start_date = add_zero(start_date) #convert to str and add 0 to start if one digit
-        end_date = add_zero(end_date) #same as above
-        query = f"{company} lang:en until:2021-{end_date}-01 since:2021-{start_date}-01" #the query submitted to twitter
-        for index, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
-            tweets.append([date_index+1, tweet.content]) #appends each tweet we get to the list with the month it's from
-            if index == limit-1: #stop once the limit has been reached
-                break
-    tweets = transpose(tweets) #pandas wants them transposed
-    tweet_df = pd.DataFrame(tweets, ['Month', 'Content'])
-    tweet_df = tweet_df.transpose() #transpose the df to get them back to how they were
-    tweet_df.to_csv(f'{company}.csv') #write to output file
+if __name__ == '__main__':
+    main()
--- public_opinion/get_results.py
+++ public_opinion/get_results.py
@@ -1,32 +1,36 @@
-from transformers import pipeline
-import pandas as pd
-from statistics import mean, stdev
+def main():
+    from transformers import pipeline
+    import pandas as pd
+    from statistics import mean, stdev
+    
+    companies = ['Google', 'Tesla', 'Microsoft']
+    months = [x+1 for x in range(11)]
+    classifier = pipeline(task='sentiment-analysis') #nlp model that classifies text as either positive or negative
+    
+    output_df = pd.DataFrame() #initialize the final dataframe
+    output_df['Months'] = months #add months
+    
+    for company in companies:
+        tweet_df = pd.read_csv(f'{company}.csv') #read the raw twitter data
+        mean_list = [] #list of means
+        stdev_list = [] #list of standard deviations
+        for month in months:
+            month_tweets = tweet_df.loc[tweet_df['Month'] == month] #get all rows that correspond to a certain month
+            scores = classifier(month_tweets['Content'].to_list()) #create a list of scores
+            
+            #the next three lines put scores on a scale from 0 (negative) to 1 (positive)
+            pos_scores = [item['score'] for item in scores if item['label'] == 'POSITIVE']
+            neg_scores = [1-item['score'] for item in scores if item['label'] == 'NEGATIVE']
+            scores = pos_scores + neg_scores
+            
+            mean_score = mean(scores) #get the mean score
+            score_stdev = stdev(scores) #get the standard deviation
+            mean_list.append(mean_score) #add the mean score to the list
+            stdev_list.append(score_stdev) #add the standard deviation to the list
+        output_df[f'{company} Mean'] = mean_list #add new column to df with means for each month
+        output_df[f'{company} Standard Deviation'] = stdev_list #do the same as above with standard deviations
+    
+    output_df.to_csv('final_data.csv') #write the ouput to a csv file
 
-companies = ['Google', 'Tesla', 'Microsoft']
-months = [x+1 for x in range(11)]
-classifier = pipeline(task='sentiment-analysis') #nlp model that classifies text as either positive or negative
-
-output_df = pd.DataFrame() #initialize the final dataframe
-output_df['Months'] = months #add months
-
-for company in companies:
-    tweet_df = pd.read_csv(f'{company}.csv') #read the raw twitter data
-    mean_list = [] #list of means
-    stdev_list = [] #list of standard deviations
-    for month in months:
-        month_tweets = tweet_df.loc[tweet_df['Month'] == month] #get all rows that correspond to a certain month
-        scores = classifier(month_tweets['Content'].to_list()) #create a list of scores
-        
-        #the next three lines put scores on a scale from 0 (negative) to 1 (positive)
-        pos_scores = [item['score'] for item in scores if item['label'] == 'POSITIVE']
-        neg_scores = [1-item['score'] for item in scores if item['label'] == 'NEGATIVE']
-        scores = pos_scores + neg_scores
-        
-        mean_score = mean(scores) #get the mean score
-        score_stdev = stdev(scores) #get the standard deviation
-        mean_list.append(mean_score) #add the mean score to the list
-        stdev_list.append(score_stdev) #add the standard deviation to the list
-    output_df[f'{company} Mean'] = mean_list #add new column to df with means for each month
-    output_df[f'{company} Standard Deviation'] = stdev_list #do the same as above with standard deviations
-
-output_df.to_csv('final_data.csv') #write the ouput to a csv file
+if __name__ == '__main__':
+    main()
