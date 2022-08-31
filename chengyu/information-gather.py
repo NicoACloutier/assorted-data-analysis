4b074749798484acb1a6fc8098dee2eb188c6311
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-08-30 20:40:42 -0400

    add definition column

--- chengyu/information-gather.py
+++ chengyu/information-gather.py
@@ -1,11 +1,6 @@
 def main():
     import pandas as pd
-    from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
     import re
-    
-    model = AutoModelForSeq2SeqLM.from_pretrained('Helsinki-NLP/opus-mt-zh-en')
-    tokenizer = AutoTokenizer.from_pretrained('Helsinki-NLP/opus-mt-zh-en')
-    translate = pipeline('translation', model=model, tokenizer=tokenizer)
 
     chengyu = pd.read_csv('chengyu-appearances.csv')
     appearing_idioms = chengyu['Chengyu'].to_list()
@@ -16,8 +11,6 @@ def main():
     raw_text = file.read()
     chengyu_list = re.findall('(?<=<div id=""ent"">)....(?=</div> <div id=""from"">)', raw_text)
     definition_list = re.findall('(?<=<span id=""from_content"">).+?(?=</span> </div>)', raw_text)
-    print(len(chengyu_list))
-    print(len(definition_list))
     meaning_dict = dict()
     for i, chengyu in enumerate(chengyu_list):
         definition = definition_list[i]
@@ -27,10 +20,6 @@ def main():
     for idiom in appearing_idioms:
         meaning = meaning_dict[idiom]
         definition_list.append(meaning)
-    
-    english_list = translate(definition_list)
-    
-    print(english_list[0])
 
 
 if __name__ == '__main__':
