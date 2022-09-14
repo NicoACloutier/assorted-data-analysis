8ae763338f260265691cbe06050be9d44948e4aa
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-09-14 13:20:10 -0400

    add basic info

--- city_health/analysis.jl
+++ city_health/analysis.jl
@@ -4,7 +4,6 @@ using DataFrames
 
 #get basic statistical information for a vector of data
 function basicinfo(name, column)
-	#column = removemissing(column)
 	Dict("name"=>name,
 	"mean"=>mean(column),
 	"stdev"=>stdm(column, mean(column)),
@@ -46,6 +45,40 @@ function correlationtodf(correlations)
 	"FIRST"=>firstlist, "SECOND"=>secondlist), 1)
 end
 
+#find the diseases with the highest correlation to another in a correlation df
+function mostcorrelated(corrdf)
+	mostcorr = []
+	rows = copy.(eachrow(corrdf))
+	namelist = unique([item.FIRST for item in rows])
+	for name in namelist
+		for row in rows
+			if row.FIRST == name
+				push!(mostcorr, row.SECOND)
+				break
+			end
+		end
+	end
+	(namelist, mostcorr)
+end
+
+function getrows(namelist, dictlist)
+	meanlist = []
+	stdevlist = []
+	varlist = []
+	for name in namelist
+		for dict in dictlist
+			if dict["name"] == "$(uppercase(name))_CrudePrev"
+				global currentdict = dict
+				break
+			end
+		end
+		push!(meanlist, currentdict["mean"])
+		push!(stdevlist, currentdict["stdev"])
+		push!(varlist, currentdict["var"])
+	end
+	(meanlist, stdevlist, varlist)
+end
+
 #put the disease names in a more presentable form
 clean(diseasename) = lowercase(replace(diseasename, "_CrudePrev"=>""))
 
@@ -64,3 +97,12 @@ outputdf = correlationtodf(sorted) #make output dataframe
 CSV.write("correlations.csv", outputdf) #write to csv
 
 columninfo = getbasic(columns, df, rx) #get basic information for columns with numerical info in df
+(newcolumns, mostcorr) = mostcorrelated(outputdf) #find most correlated columns
+(means, stdevs, vars) = getrows(newcolumns, columninfo) #get basic info
+#put info in df
+basicdf = DataFrame("NAME"=>newcolumns, 
+					"MOST CORRELATED"=>mostcorr, 
+					"MEAN"=>means, 
+					"STANDARD DEVIATION"=>stdevs, 
+					"VARIANCE"=>vars)
+CSV.write("basicinfo.csv", basicdf) #write to csv
