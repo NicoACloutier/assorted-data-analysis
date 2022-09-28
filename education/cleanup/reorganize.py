c79983e44e36ae9afba3836cecfcbeb1d3ed9def
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-09-28 12:06:51 -0400

    fully reorganize raw data

--- education/cleanup/reorganize.py
+++ education/cleanup/reorganize.py
@@ -2,6 +2,8 @@ def main():
     import pandas as pd
     import re
     
+    #REORGANIZE TEST DATA
+    
     replace_dict = {
     'elementary school': 'es',
     'middle school': 'ms',
@@ -12,6 +14,11 @@ def main():
     '-': ' ',
     'high': 'h',
     '.': ' ',
+    ' es': '',
+    ' ms': '',
+    ' hs': '',
+    ' ec': '',
+    ' bs': '',
     '  ': ' ',
     }
     
@@ -30,11 +37,16 @@ def main():
     
     #normalize the school names
     def normalize(school_name, replace_dict):
+        if school_name == float('NaN'):
+            school_name = 'empty'
         school_name = school_name.lower()
         for key in replace_dict:
             school_name = school_name.replace(key, replace_dict[key]) #normalize school names
         if '@' in school_name: #get rid of @
             ind = school_name.index('@')
+            school_name = school_name[:ind-1]
+        if ' at ' in school_name: #get rid of at
+            ind = school_name.index(' at ')
             school_name = school_name[:ind-1]
         school_name = re.sub(' ?\(.+?\)', '', school_name) #get rid of parentheses
         return school_name
@@ -49,12 +61,15 @@ def main():
         columns.remove('Year') #remove year from columns
         return_df = pd.DataFrame() #df to be returned
         for year in years:
-            averages = pd.DataFrame(columns = ["Name", "Column", "Average", "Year"]) #dataframe with column names and school names
+            df_columns = ["Name", "Year"] + columns
+            averages = pd.DataFrame(columns = df_columns) #dataframe with column names and school names
             even_more_temp_df = temp_df[temp_df['Year'] == year] #whittle df
             for i, column in enumerate(columns):
                 values = even_more_temp_df[column] #get values for that year and that column
                 average = sum(values) / len(values) #get average values
-                averages.loc[i] = pd.Series({"Name": name, "Column": column, "Average": average, "Year": year})
+                averages[column] = [average]
+            averages["Name"] = name
+            averages["Year"] = year
             return_df = pd.concat([return_df, averages])
         return return_df
     
@@ -68,7 +83,6 @@ def main():
         temp_df['Year'] = years #make year column
         test_df = pd.concat([test_df, temp_df]) #add to test df
     
-    
     schools = list(set(test_df['School Name ']))
     
     averages = pd.DataFrame()
@@ -76,7 +90,58 @@ def main():
         school_df = school_average(school, test_df, replace_dict)
         averages = pd.concat([school_df, averages])
 
-    averages.to_csv('raw\\test.csv', index=False)
+    averages = averages.sort_values(['Name', 'Year'])
+    averages.to_csv('test.csv', index=False)
+    
+    #REORGANIZE BUDGET DATA
+    
+    #open up a budget csv file
+    def open_budget(budget, budget_dict, year):
+        (name_id, budget_id) = budget_dict[budget] #get information on column names for school name and budget allocation
+        df = pd.read_csv(f'raw\\{budget}.csv', low_memory=False) #read budget csv
+        return_df = pd.DataFrame(columns=['Name', 'Budget', 'Year']) #df to be returned
+        df = df[[name_id, budget_id]]
+        df = df.dropna()
+        return_df['Name'] = pd.Series([normalize(name, replace_dict) for name in df[name_id]], dtype='string') #get and normalize school name
+        return_df['Budget'] = pd.Series(df[budget_id], dtype='float32') #get budget id
+        return_df['Year'] = pd.Series([year for _ in range(len(df[name_id]))], dtype='category') #add year column
+        return return_df
+    
+    #combine the budget within a single school and year
+    def combine_budget(budget_df):
+        return_df = pd.DataFrame(columns=['Name', 'Budget', 'Year'])
+        schools = list(budget_df['Name'].unique()) #list of unique schools
+        for school in schools:
+            school_list = []
+            temp_df = budget_df[budget_df['Name'] == school]
+            years = list(temp_df['Year'].unique())
+            for year in years:
+                even_more_temp_df = temp_df[temp_df['Year'] == year]
+                overall = sum(list(even_more_temp_df['Budget']))
+                school_list.append({'Name': school, 'Budget': overall, 'Year': year})
+            school_df = pd.DataFrame(school_list)
+            return_df = pd.concat([return_df, school_df])
+        return return_df
+    
+    budget_dict = {
+    'b14': ('School', 'Amount'),
+    'b15': ('school_name', 'total_cost'),
+    'b16': ('school_name', 'amount'),
+    'b17': ('School Name', 'Total School Budget Allocation'),
+    }
+    
+    budgets = [f'b1{4+i}' for i in range(4)] #budget files
+    
+    #make dataframes of budgets
+    budget_df = pd.DataFrame(columns=['Name', 'Budget', 'Year'])
+    for i, budget in enumerate(budgets):
+        year = f'201{4+i}'
+        temp_df = open_budget(budget, budget_dict, year) #open budget file
+        budget_df = pd.concat([temp_df, budget_df]) #add to budget df
+    
+    budget_df = combine_budget(budget_df) #combine the budget for each year and each school
+    budget_df = budget_df.sort_values(['Name', 'Year'])
+    budget_df.to_csv('budget.csv', index=False)
 
 if __name__ == '__main__':
     main()
