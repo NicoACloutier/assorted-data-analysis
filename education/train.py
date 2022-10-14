5450afd3289fabed4b7b930e268eaee2737f3750
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-10-14 08:58:18 -0400

    build model

--- education/train.py
+++ education/train.py
@@ -1,29 +1,40 @@
 import torch
-from torch import nn, as_tensor
+from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import Dataset
+from torch.utils import data
 
-get_fields = lambda line, fields: [line[field] for field in fields]
+get_fields = lambda line, fields: [line[field] for field in fields] #get given fields in a line
 
+#split the dataframe by fields and into train/test
 def field_split(df, inputs, outputs, percentage):
-    inputs = get_fields(df, inputs)
-    outputs = get_fields(df, outputs)
-    partway = (len(df) * percentage) // 100
-    train_inputs = inputs[:partway]
-    train_outputs = outputs[:partway]
-    test_inputs = inputs[partway:]
-    test_outputs = outputs[partway:]
+    inputs = [get_fields(line, inputs) for _, line in df.iterrows()] #get the input fields
+    outputs = [get_fields(line, outputs) for _, line in df.iterrows()] #get the output fields
+    partway = (len(df) * percentage) // 100 #get the integer value of a given percentage into the df
+    train_inputs = inputs[:partway] #set the train inputs equal to the first percentage
+    train_outputs = outputs[:partway] #set the train outputs equal to the second percentage
+    test_inputs = inputs[partway:] #set the test inputs equal to the second percentage
+    test_outputs = outputs[partway:] #set the test outputs equal to the second percentage
     return train_inputs, train_outputs, test_inputs, test_outputs
 
-class SubDataset(Dataset):
-    def __init__(self, sequences, targets, test_sequences, test_targets):
-        self.targets = targets
-        self.sequences = sequences
-        self.test_seq = test_sequences
-        self.test_tar = test_targets
+class SubDataset(data.Dataset):
+    def __init__(self, sequences, targets, test_sequences, test_targets, device):
+        self.targets = torch.tensor(targets, device=device, dtype=torch.double) #set targets
+        self.sequences = torch.tensor(sequences, device=device, dtype=torch.double) #set sequences
+        self.test_seq = torch.tensor(test_sequences, device=device, dtype=torch.double) #set test sequences
+        self.test_tar = torch.tensor(test_targets, device=device, dtype=torch.double) #set test targets
 
-    def __getitem__(self, i): return [self.sequences[i], self.targets[i]]
+    def __getitem__(self, i): return (self.sequences[i], self.targets[i])
     def __len__(self): return len(self.sequences)
+        
+    def __iter__(self):
+        self.i = -1
+        return self
+        
+    def __next__(self):
+        self.i += 1
+        if self.i < len(self):
+            return self[self.i]
+        raise StopIteration
 
 class Model(nn.Module):
     def __init__(self, input_size, output_size):
@@ -37,16 +48,6 @@ class Model(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.sigmoid(self.fc3(x))
         return x
-        
-    def __iter__(self):
-        self.i = -1
-        return self
-        
-    def __next__(self):
-        self.i += 1
-        if self.i < len(self):
-            return self[self.i]
-        raise StopIteration
 
 def main():
     import pandas as pd
@@ -65,10 +66,10 @@ def main():
     test_inputs, test_outputs) = field_split(df, input_fields, 
                                              output_fields, percentage)
     
-    schools = SubDataset(train_inputs, train_outputs, test_inputs, test_outputs)
+    schools = SubDataset(train_inputs, train_outputs, test_inputs, test_outputs, device)
     input_size = len(schools.sequences[0])
     output_size = len(schools.targets[0])
-    model = Model(input_size, output_size)    
+    model = Model(input_size, output_size).double()
     num_epochs = 3
     batch_size = 500
     learning_rate = 0.01
