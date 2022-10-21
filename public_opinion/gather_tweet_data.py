6eea037932b70bc48eccc195aba98a8ecc15cfe4
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-10-21 12:54:29 -0400

    add raw wage data

--- public_opinion/gather_tweet_data.py
+++ public_opinion/gather_tweet_data.py
@@ -1,32 +1,34 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+LIMIT = 10000
+
+#transpose a matrix
+def transpose(matrix):
+    num_rows = len(matrix[0])
+    num_columns = len(matrix)
+    Final = [[0]*num_columns for x in range(num_rows)]
+    for x in range(num_rows):
+        for i in range(num_columns):
+            Final[x][i] = matrix[i][x]
+    return Final
+    
+#change an int to a string. if it's only made up of one character,
+#add a zero to the beginning. (this is because of the way twitter 
+#handles dates)
+def add_zero(date):
+    date = str(date)
+    if len(date) == 1:
+        date = '0' + date
+    return date
+
 def main():
-    import snscrape.modules.twitter as sntwitter
-    import pandas as pd
-    
-    #transpose a matrix
-    def transpose(matrix):
-        num_rows = len(matrix[0])
-        num_columns = len(matrix)
-        Final = [[0]*num_columns for x in range(num_rows)]
-        for x in range(num_rows):
-            for i in range(num_columns):
-                Final[x][i] = matrix[i][x]
-        return Final
-    
-    #change an int to a string. if it's only made up of one character,
-    #add a zero to the beginning. (this is because of the way twitter 
-    #handles dates)
-    def add_zero(date):
-        date = str(date)
-        if len(date) == 1:
-            date = '0' + date
-        return date
     
     companies = ["Tesla", "Microsoft", "Google"]
     
     #get a range of numbers from 1 to 11 for the months we're gathering data on
     start_dates = [x+1 for x in range(11)]
     
-    limit = 1000 #the number of tweets collected per company per month
     for company in companies:
         tweets = []
         for date_index, start_date in enumerate(start_dates):
@@ -36,7 +38,7 @@ def main():
             query = f"{company} lang:en until:2021-{end_date}-01 since:2021-{start_date}-01" #the query submitted to twitter
             for index, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
                 tweets.append([date_index+1, tweet.content]) #appends each tweet we get to the list with the month it's from
-                if index == limit-1: #stop once the limit has been reached
+                if index == LIMIT-1: #stop once the limit has been reached
                     break
         tweets = transpose(tweets) #pandas wants them transposed
         tweet_df = pd.DataFrame(tweets, ['Month', 'Content'])
