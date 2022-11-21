485c30039829320296f3e1b2a050b1d61ed26cb1
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-11-21 12:31:24 -0500

    document aggregator script

--- population/aggregate.py
+++ population/aggregate.py
@@ -4,7 +4,7 @@ import random
 BASIC = '.\\countries'
 
 def main():
-    indicators = ['gdp', 'military_percent', 'bribery', 'top_share']
+    indicators = ['gdp', 'military_percent', 'bribery', 'top_share'] #variables to be predicted by model
     
     df = pd.read_csv(f'{BASIC}\\population.csv')
     for indicator in indicators:
@@ -17,6 +17,11 @@ def main():
             year = row['Year']
             country_indicator_df = indicator_df[indicator_df['Country'] == country]
             temp_indicator_df = country_indicator_df[country_indicator_df['Year'] == year]
+            
+            #set the value to be added equal to the value of that country for that year.
+            #if there is no such value, select a random value from that country in some year.
+            #if that country has no data for any year, set the value equal to the average value
+            #for the whole world.
             if len(temp_indicator_df) == 1:
                 value = temp_indicator_df['Value'].item()
             else:
@@ -25,6 +30,7 @@ def main():
                     value = random.choice(temp_values)
                 else:
                     value = avg_value
+            
             indicator_list.append(value)
         df[indicator] = indicator_list
     
