527a65b8ed23cb654b6e5aa4708e6ed3232bcd36
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2022-12-30 19:31:49 -0700

    add gaussian nb classifier

---
+++ c_models/gnbc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+//GAUSSIAN NAÏVE BAYES CLASSIFIER
+
+struct Gaussian {
+	double mean;
+	double std_dev;
+}
+
+struct MultipleGaussian {
+	int dimensions;
+	struct Gaussian *gaussians;
+}
