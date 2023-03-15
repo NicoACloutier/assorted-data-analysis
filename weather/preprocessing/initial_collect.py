b750f6f76670bbd26b55e5d4653c4027f98f72c1
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2023-03-15 13:26:35 -0400

    flesh out css selectors

--- weather/preprocessing/initial_collect.py
+++ weather/preprocessing/initial_collect.py
@@ -1,24 +1,28 @@
 import pandas as pd
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import urllib
+import time
 import numpy as np
 
-driver = webdriver.Chrome()
 URL = 'https://disc.gsfc.nasa.gov/datasets/M2I3NPASM_5.12.4/summary'
-FRACTION = 10 #fraction of data to be kept, in form 1/FRACTION
+FRACTION = 100 #fraction of data to be kept, in form 1/FRACTION
 DAYS_SURROUNDING = 4 #how many days surrounding the central day will be recorded on each side 
 OUTPUT_FILE = '..\\data\\raw\\raw.csv'
+CHROMEDRIVER_PATH = 'C:\\Users\\nicoc\\Documents\\chromedriver.exe'
 
 #css selectors, name of variable is what button says
-SUBSET_GET_DATA = ('btn.btn-xs.btn-link.borderless.pop.ng-scope', 0)
-DOWNLOAD_METHOD = ('accordion-toggle', 0)
-GET_FILE_SUBSETS_USING_OPENDAP = ('ng-valid.ng-not-empty.ng-dirty.ng-valid-parse.ng-touched', 1)
-VARIABLES = ('accordion-toggle', 3)
-FILE_FORMAT = ('accordion-toggle', 0)
-ASCII = ('ng-valid.ng-not-empty.ng-dirty.ng-valid-parse.ng-touched', 0)
-GET_DATA = ('btn.btn-success.modal-footer-btn', 0)
-DOWNLOAD_LINK_LIST = ('download-button.download-link-disabled', 0)
+SUBSET_GET_DATA = ('.btn.btn-xs.btn-link.borderless.pop.ng-scope', 0)
+DOWNLOAD_METHOD = ('.accordion-toggle', 0)
+GET_FILE_SUBSETS_USING_OPENDAP = ('.ng-pristine.ng-untouched.ng-valid.ng-not-empty', 1)
+VARIABLES = ('.accordion-toggle', 3)
+FILE_FORMAT = ('.accordion-toggle', 4)
+ASCII = ('.ng-pristine.ng-untouched.ng-valid.ng-not-empty', 0)
+GET_DATA = ('.btn.btn-success.modal-footer-btn', 0)
+DOWNLOAD_LINK_LIST = ('.download-button.download-link-disabled', 0)
+
+wait = lambda: time.sleep(5)
 
 #parse the ascii text data from NASA data website
 def parse_ascii(text):
@@ -49,27 +53,28 @@ def to_df(array, start_columns):
 
 #click on each of the buttons in a list
 def click_buttons(driver, button_list):
-    for (selector, index) in button_selectors:
-        driver.find_elements_by_css_selector(selector)[index].click()
-        
-        #close dropdown if the item is a dropdown
-        if selector == 'accordion-toggle':
-            driver.find_elements_by_css_selector(selector)[index].click()
+    for (selector, index) in button_list:
+        element = driver.find_elements(By.CSS_SELECTOR, selector)[index]
+        print(element)
+        element.click()
+        wait()
     
 
 def main():
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH)
     driver.get(URL)
-    button_selectors = [SUBSET_GET_DATA, DOWNLOAD_METHOD, GET_FILE_SUBSETS_USING_OPENDAP] #initial buttons
+    wait()
+    button_selectors = [SUBSET_GET_DATA, DOWNLOAD_METHOD, GET_FILE_SUBSETS_USING_OPENDAP, DOWNLOAD_METHOD, VARIABLES] #initial buttons
     
     click_buttons(driver, button_selectors) #click the initial buttons
     
-    #click on all of the variables
-    driver.find_elements_by_css_selector(VARIABLES[0])[VARIABLES[1]].click()
-    for element in driver.find_elements_by_css_selector('checkbox-inline'):
+    #click all variable names
+    wait()
+    for element in driver.find_elements(By.CSS_SELECTOR, '.checkbox-inline'):
         element.click()
-    driver.find_elements_by_css_selector(VARIABLES[0])[VARIABLES[1]].click()
+    wait()
     
-    click_buttons(driver, [FILE_FORMAT, ASCII, GET_DATA]) #click on the final buttons
+    click_buttons(driver, [VARIABLES, FILE_FORMAT, ASCII, FILE_FORMAT, GET_DATA]) #click on the final buttons
     
     links = string(drive.find_element_by_tag_name('body')).split('\n')[1:] #first link is readme, ignore
     driver.quit()
