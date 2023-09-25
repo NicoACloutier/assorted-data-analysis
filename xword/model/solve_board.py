7536684db78dd2b3f91ba7078e8679a7ad8157b3
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2023-09-24 20:45:12 -0400

    assorted bugfixes

--- xword/model/solve_board.py
+++ xword/model/solve_board.py
@@ -28,7 +28,10 @@ def is_across_word_start(representation: str, current_char: int, line_length: in
     Returns:
         `bool`: whether the character is the start of an across word.
     '''
-    return current_char % line_length == 0 or representation[current_char-1] == '!'
+    if current_char >= len(representation):
+        return False
+    else:
+        return current_char % line_length == 0 or representation[current_char-1] == '!'
     
 
 def is_down_word_start(representation: str, current_char: int, line_length: int) -> bool:
@@ -41,7 +44,10 @@ def is_down_word_start(representation: str, current_char: int, line_length: int)
     Returns:
         `bool`: whether the character is the start of a down word.
     '''
-    return current_char < line_length or representation[current_char-line_length] == '!'
+    if current_char >= len(representation):
+        return False
+    else:
+        return current_char < line_length or representation[current_char-line_length] == '!'
 
 def find_rep_index(representation: str, number: int, line_length: int) -> int:
     '''
@@ -54,9 +60,8 @@ def find_rep_index(representation: str, number: int, line_length: int) -> int:
         `int`: the index of the number in the string.
     '''
     num_count, current_char = 0, 0
-    while num_count != number:
-        if (representation[current_char] not in ['!', '\n']) and 
-            (is_across_word_start(representation, current_char, line_length) or is_down_word_start(representation, current_char, line_length)):
+    while num_count != number and current_char != len(representation):
+        if (representation[current_char] not in ['!', '\n']) and (is_across_word_start(representation, current_char, line_length) or is_down_word_start(representation, current_char, line_length)):
             num_count += 1
         current_char += 1
     return current_char
@@ -76,6 +81,23 @@ def filter_wordlist(word_rep: str, wordlist: list[str]) -> list[str]:
             wordlist = [word for word in wordlist if word[i] == char]
     return wordlist
 
+def find_word_rep(representation: str, index: int, across: bool, line_length: int) -> str:
+    '''
+    Find the representation of a word if its index is already known.
+    Arguments:
+        `representation: str`: the string representation of a crossword board.
+        `index: int`: the index of the word in the board representation.
+        `across: bool`: whether the word goes across.
+        `line_length: int`: the length of a line.
+    Returns:
+        `str`: the representation of the word alone.
+    '''
+    word_rep = ''
+    while index < len(representation) and representation[index] != '!':
+        word_rep += representation[index]
+        index += 1 if across else line_length
+    return word_rep
+
 def find_number_info(representation: str, number: int, across: bool) -> str:
     '''
     Find a word place within a crossword representation string.
@@ -90,10 +112,7 @@ def find_number_info(representation: str, number: int, across: bool) -> str:
     line_length = representation.index('\n')
     representation = representation.replace('\n', '')
     current_char = find_rep_index(representation, number, line_length)
-    while current_char < len(representation) and representation[current_char] != '!':
-        word_rep += representation[current_char]
-        current_char += 1 if across else line_length
-    return word_rep
+    return find_word_rep(representation, current_char, across, line_length)
 
 def find_closest(model: sentence_transformers.SentenceTransformer, clue: str, wordlist: list[str], embeddings: np.ndarray) -> tuple[typing.Optional[str], int]:
     '''
@@ -109,11 +128,11 @@ def find_closest(model: sentence_transformers.SentenceTransformer, clue: str, wo
     '''
     embedding = model.encode(clue)
     nearest = np.linalg.norm(embeddings - embedding, axis=1).argsort()[:NUM_WORDS]
-    item_counter = collections.Counter([wordlists[ind] for ind in nearest])
-    word = max([wordlists[ind] for ind in nearest], key=item_counter.get)
+    item_counter = collections.Counter([wordlist[ind] for ind in nearest])
+    word = max([wordlist[ind] for ind in nearest], key=item_counter.get)
     return word if item_counter[word] > 1 else None, item_counter[word]
 
-def change_rep(representation: str, numbers: list[int], i: int, line_length: int, across: bool) -> tuple[str, bool]:
+def change_rep(representation: str, numbers: list[int], i: int, line_length: int, across: bool) -> tuple[str, bool, int]:
     '''
     Change the representation string, ensuring no changes are made that conflict with past changes.
     Arguments:
@@ -125,20 +144,22 @@ def change_rep(representation: str, numbers: list[int], i: int, line_length: int
     Returns:
         `str`: the updated representation string.
         `bool`: whether the string was replaced successfully.
+        `int`: the number of the word.
     '''
     char_index = find_rep_index(representation, numbers[i], line_length)
+    word = find_word_rep(representation, char_index, across, line_length)
     bad = False
     for char in word:
-        if representation[char_index] not in ['_', '!'] + char:
+        if representation[char_index] not in ['_', '!'] + [char,]:
             bad = True
             break
     if bad:
-        return representation
+        return representation, not bad, numbers[i]
     char_index -= len(word)
     for char in word:
-        representation[char_index] = char
+        representation = representation[:char_index] + char + representation[char_index+1:]
         char_index += 1 if across else line_length
-    return representation, !bad
+    return representation, not bad, numbers[i]
 
 def model_fill(clue: str, clue_list: list[str]) -> str:
     '''
@@ -189,8 +210,9 @@ def update_rep(representation: str, current_char: int, wordlist: list[str], answ
         if len(possibilities) == 1:
             answers[number] = possibilities[0]
             del prompts[number]
-            for i, char in possiblities[0]:
-                representation[current_char + i * (1 if across else line_length)] = char
+            for i, char in enumerate(possibilities[0]):
+                index = current_char + i * (1 if across else line_length)
+                representation = representation[index:] + char + representation[index+1:]
     return prompts, answers, representation
 
 def filter_unique(representation: str, wordlist: list[str], down_prompts: dict[int, str], across_prompts: dict[int, str], 
@@ -225,11 +247,13 @@ def filter_unique(representation: str, wordlist: list[str], down_prompts: dict[i
     representation = temp_rep
     return down_answers, across_answers, down_prompts, across_prompts, representation
 
-def get_none(representation: str, wordlist: list[str]) -> tuple[dict[int, str], dict[int, str], dict[int, str], dict[int, str]]:
+def get_none(representation: str, wordlist: list[str], down_prompts: dict[int, str], across_prompts: dict[int, str]) -> tuple[dict[int, str], dict[int, str], dict[int, str], dict[int, str]]:
     '''
     Arguments:
         `representation: str`: the string representation of a crossword board.
         `wordlist: list[str]`: a list of possible words given as answers.
+        `down_prompts: dict[int, str]`: the prompts going down, with numbers.
+        `across_prompts: dict[int, str]`: the prompts going across, with numbers.
     Returns:
         `dict[int, str]`: the nones going down.
         `dict[int, str]`: the nones going across.
@@ -238,21 +262,23 @@ def get_none(representation: str, wordlist: list[str]) -> tuple[dict[int, str], 
     '''
     across_nones, down_nones = dict(), dict()
     line_length, temp_rep = representation.index('\n'), representation.replace('\n', '')
+    print(representation)
+    print(line_length)
     number = 0
     for i, char in enumerate(representation):
         across_word, down_word = is_across_word_start(temp_rep, i, line_length), is_down_word_start(temp_rep, i, line_length)
         number += int(across_word or down_word)
         if char != '!' and across_word:
-            if len(filter_wordlist(find_number_info(representation, number, True), wordlist)) == 0:
+            if len(filter_wordlist(find_number_info(representation, number, True), wordlist)) == 0 and number in across_prompts:
                 across_nones[number] = across_prompts[number]
                 del across_prompts[number]
         if char != '!' and down_word:
-            if len(filter_wordlist(find_number_info(representation, number, False), wordlist)) == 0:
-                down_nones[number] = downs_prompts[number]
+            if len(filter_wordlist(find_number_info(representation, number, False), wordlist)) == 0 and number in down_prompts:
+                down_nones[number] = down_prompts[number]
                 del down_promts[number]
     return down_nones, across_nones, down_prompts, across_prompts
 
-def get_closest_words(representation: str, down_prompts: dict[int, str], across_prompts: dict[int, str], worldist: list[str], 
+def get_closest_words(representation: str, down_prompts: dict[int, str], across_prompts: dict[int, str], wordlist: list[str], 
                         across_answers: dict[int, str], down_answers: dict[int, str]) -> tuple[dict[int, str], dict[int, str], dict[int, str], dict[int, str], str]:
     '''
     Perform the calculation of the closest words given prompts.
@@ -271,27 +297,27 @@ def get_closest_words(representation: str, down_prompts: dict[int, str], across_
         `str`: the updated representation string.
     '''
     line_length = representation.index('\n')
-    down_numbers, across_numbers = list(down_prompts.values()), list(across_prompts.values())
+    down_numbers, across_numbers = list(down_prompts.keys()), list(across_prompts.keys())
     across_clues = [find_number_info(representation, number, True) for number in across_prompts]
     down_clues = [find_number_info(representation, number, False) for number in down_prompts]
     model = sentence_transformers.SentenceTransformer(MODEL)
     with open(EMBED_FILE, 'rb') as f:
         embeddings = pickle.loads(f.read())
-    across_answers = [find_closest(model, clue, wordlist, embeddings) for clue in across_clues]
-    down_answers = [find_closest(model, clue, wordlist, embeddings) for clue in down_clues]
-    for i in  for x in range(1, NUM_WORDS+1)[::-1]:
-        temp_across_answers = [answer for answer in across_answers if answer[1] == i]
-        temp_down_answers = [answer for answer in down_answers if answer[1] == i]
+    across_answers = {clue: find_closest(model, clue, wordlist, embeddings) for clue in across_clues}
+    down_answers = {clue: find_closest(model, clue, wordlist, embeddings) for clue in down_clues}
+    for x in range(1, NUM_WORDS+1)[::-1]:
+        temp_across_answers = [answer for answer in across_answers if across_answers[answer][1] == x]
+        temp_down_answers = [answer for answer in down_answers if down_answers[answer][1] == x]
         for i, word in enumerate(temp_down_answers):
-            representation, good = change_rep(representation, down_numbers, i, line_length, False)
+            representation, good, number = change_rep(representation, down_numbers, i, line_length, False)
             if good:
-                down_answers[i] = word
-                del down_prompts[i]
+                down_answers[number] = word
+                del down_prompts[number]
         for i, word in enumerate(temp_across_answers):
-            representation, good = change_rep(representation, across_numbers, i, line_length, True)
+            representation, good, number = change_rep(representation, across_numbers, i, line_length, True)
             if good:
-                across_answers[i] = word
-                del across_prompts[i]
+                across_answers[number] = word
+                del across_prompts[number]
     return down_answers, across_answers, down_prompts, across_prompts, representation
 
 def solve_board(representation: str, down_prompts: dict[int, str], across_prompts: dict[int, str]) -> tuple[dict[int, str], dict[int, str]]:
@@ -305,14 +331,20 @@ def solve_board(representation: str, down_prompts: dict[int, str], across_prompt
         `dict[int, str]`: the answers going down.
         `dict[int, str]`: the answers going across.
     '''
+    print(representation)
+    df = pd.read_csv('../data/xword.csv')
     wordlist = list(df['Answer'].astype(str))
     previous_length, current_length, none_list = -1, 0, []
+    down_answers, across_answers = dict(), dict()
     while (down_prompts or across_prompts) and current_length != previous_length:
-        down_answers, across_answers, down_prompts, across_prompts, representation = get_closest_words(representation, down_prompts, across_prompts, wordlist)
+        print(representation)
+        down_answers, across_answers, down_prompts, across_prompts, representation = get_closest_words(representation, down_prompts, across_prompts, wordlist, down_answers, across_answers)
+        print(representation)
         down_answers, across_answers, down_prompts, across_prompts, representation = filter_unique(representation, wordlist, down_prompts, across_prompts, down_answers, across_answers)
         down_nones, across_nones, down_prompts, across_prompts = get_none(representation, wordlist, down_prompts, across_prompts)
         previous_length = current_length
         current_length = len(down_answers) + len(across_answers)
+    print(representation)
     for prompt in down_nones:
         down_prompts[prompt] = down_nones[prompt]
     for prompt in across_prompts:
